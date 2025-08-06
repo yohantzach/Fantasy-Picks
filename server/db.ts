e22cb912ca806a051091680e02f22de3a3f6d9ba
yohantzach <115889909+yohantzach@users.noreply.github.com>
2025-08-06 23:54:27 +0000

    fix: for db connection

--- server/db.ts
+++ server/db.ts
@@ -17,7 +17,7 @@ const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === "development"
       ? { rejectUnauthorized: false }
       : false,
 });
